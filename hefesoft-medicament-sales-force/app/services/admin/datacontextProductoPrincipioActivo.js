cd5d6c64d95faccee3808cc7d662034d2296b857
Jose Douglas <chibchombiano26@hotmail.com>
2014-02-16 14:30:56 -0500

    Productos

---
+++ hefesoft-medicament-sales-force/app/services/admin/datacontextProductoPrincipioActivo.js
@@ -0,0 +1,99 @@
+(function () {
+    'use strict';
+
+    var serviceId = 'datacontextProductoPrincipioActivo';
+    angular.module('app').factory(serviceId,
+        ['common', 'AzureMobileClient', datacontextProductoPrincipioActivo]);
+
+    function datacontextProductoPrincipioActivo(common, AzureMobileClient) {
+        var $q = common.$q;
+
+        var dataSource = new kendo.data.DataSource({
+            transport: {
+                read: function (options) { dataSourceRead(options); },
+                create: function (options) { dataSourceCreate(options) },
+                update: function (options) { dataSourceUpdate(options) },
+                destroy: function (options) { dataSourceDestroy(options) },
+            },
+            schema: {
+                model: {
+                    id: "id",
+                    fields: {
+                        id: { editable: false, validation: { required: false } },
+                        descripcion: { type: "string", validation: { required: true } },                        
+                    }
+                }
+            }
+        });
+
+        var service = {            
+            dataSource: dataSource
+        };
+
+        return service;
+
+        function get() {
+            var deferred = $q.defer();
+            AzureMobileClient.getAllData('tp_producto_principio_activo', 50).then(
+                    function (resultado) {
+                        deferred.resolve(resultado);
+                    },
+                    function (error) {
+                        deferred.reject(error);
+                    }
+                );
+
+            return deferred.promise;
+        }
+
+
+        function dataSourceRead(options) {
+            get().then(
+                        function (result) {                            
+                            options.success(result);
+                        },
+                        function (error) {
+                            options.error(error);
+                        }
+                        );
+        };
+
+        function dataSourceCreate(options) {
+            var item = options.data;
+            AzureMobileClient.addDataAsync("tp_producto_principio_activo", item).then(
+                function(result){
+                    options.success(result);
+                    },
+                function (err) {
+                     options.error();
+                    }
+                );            
+        };
+
+        function dataSourceUpdate(options) {
+            var item = options.data;            
+            AzureMobileClient.updateDataAsync("tp_producto_principio_activo", item).then(
+                function (result) {
+                    options.success(result);
+                },
+                function (err) {
+                    options.error();
+                }
+                );
+        };
+
+        function dataSourceDestroy(options) {
+            var item = new Object();
+            item.id = options.data.id;
+
+            AzureMobileClient.deleteDataAsync("tp_producto_principio_activo", item).then(
+                function (result) {
+                    options.success(item.id);
+                },
+                function (err) {
+                    options.error();
+                }
+                );
+        };
+    }
+})();

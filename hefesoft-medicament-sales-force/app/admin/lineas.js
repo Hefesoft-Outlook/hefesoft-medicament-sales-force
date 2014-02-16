00edd7cb993d49d5bfe3fbc275d79407dcba042d
Jose Douglas <chibchombiano26@hotmail.com>
2014-02-16 15:05:52 -0500

    Lineas

---
+++ hefesoft-medicament-sales-force/app/admin/lineas.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'lineas';
+    angular.module('app').controller(controllerId, ['common', '$scope', 'datacontextLinea', lineas]);
+
+    function lineas(common, $scope, datacontextLinea) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Lineas';
+        vm.filaSeleccionada = null;
+        vm.lineasDataSource = datacontextLinea.dataSource;
+
+        vm.columns = [            
+            { hidden: true, field: "id" },
+            { field: "nombre", title: "Nombre" },            
+            { command: ["edit", "destroy"] }
+        ];
+
+        vm.gridOpts = {
+            columns: vm.columns,
+            filterable: { extra: false },
+            pageable: false,          
+            reorderable: true,
+            sortable: true,
+            editable: {
+                mode: "popup",
+                //createAt: "top"
+            },
+            mobile: true,
+            height: "24em",
+            resizable: true,
+            toolbar: [
+                { name: "create" }                
+            ]        
+        };
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function (result) {                   
+                    log('Parametrizar Lineas');
+                });
+        }
+    }
+})();
---
+++ hefesoft-medicament-sales-force/app/admin/pais.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'pais';
+    angular.module('app').controller(controllerId, ['common', '$scope', 'datacontextLinea', pais]);
+
+    function pais(common, $scope, datacontextPais) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Pais';
+        vm.filaSeleccionada = null;
+        vm.paisDataSource = datacontextPais.dataSource;
+
+        vm.columns = [            
+            { hidden: true, field: "id" },
+            { field: "nombre", title: "Nombre" },
+            { field: "nodoPais", title: "Nodo" },
+            { command: ["edit", "destroy"] }
+        ];
+
+        vm.gridOpts = {
+            columns: vm.columns,
+            filterable: { extra: false },
+            pageable: false,          
+            reorderable: true,
+            sortable: true,
+            editable: {
+                mode: "popup",
+                //createAt: "top"
+            },
+            mobile: true,
+            height: "24em",
+            resizable: true,
+            toolbar: [
+                { name: "create" }                
+            ]        
+        };
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function (result) {                   
+                    log('Parametrizar Paises');
+                });
+        }
+    }
+})();
